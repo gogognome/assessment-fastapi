9bad9a987c403868bae6fd76fd60d20ec6d2031d
Sander Kooijmans <sander@gogognome.nl>
2025-05-06 19:38:30 +0200

    Migrate to SQLAlchemy 2 types and functions
    This also fixes the Mypy errors.

--- songs_api/models.py
+++ songs_api/models.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from sqlalchemy import Column, Integer, SmallInteger, String
-from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
-from sqlalchemy.orm import Mapped
+from sqlalchemy import Integer, SmallInteger, String
+from sqlalchemy.orm import DeclarativeBase, mapped_column
 
-Base: type[DeclarativeMeta] = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
 
 
 class Song(Base):
@@ -12,19 +13,19 @@ class Song(Base):
 
     __tablename__ = "song"
 
-    id: Mapped[int] = Column(Integer, primary_key=True)
+    id = mapped_column(Integer, primary_key=True)
     """The id of the song."""
 
-    title: Mapped[str] = Column(String(100), nullable=False)
+    title = mapped_column(String(100), nullable=False)
     """The title of the song."""
 
-    composer: Mapped[str | None] = Column(String(100))
+    composer = mapped_column(String(100))
     """The composer of the song."""
 
-    artist: Mapped[str | None] = Column(String(100))
+    artist = mapped_column(String(100))
     """The artist that performs the song."""
 
-    year_of_release: Mapped[int] = Column(SmallInteger, nullable=False)
+    year_of_release = mapped_column(SmallInteger, nullable=False)
     """
     The year that the song was released. If the song was released more than once,
     then this is the year it was released for the first time.
