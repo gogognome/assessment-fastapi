910d4b07f57a7a3b5c7ee77303ffbb405a245934
Sander Kooijmans <sander@gogognome.nl>
2025-05-07 10:58:55 +0200

    Improve error handling

--- songs_api/main.py
+++ songs_api/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from pydantic import ValidationError
 
 from songs_api.config import load_config
 from songs_api.database_access import DatabaseAccess
@@ -8,8 +7,7 @@ from songs_api.endpoints import build_router
 try:
     config = load_config("config.yml")
     database_access = DatabaseAccess(config)
-    # TODO:Test what happens when an invalid URL is passed. Does it raise a different exception than ValidationError?
-except ValidationError as e:
+except BaseException as e:
     print(f"A problem occurred while reading the configuration file: {e}")
     exit(1)
 
